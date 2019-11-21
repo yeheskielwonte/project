828ab0879368fdb49d2eb5901cb2c2fb44675217
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2019-11-21 23:15:44 +0800

    Update

--- navigator/StackNav.js
+++ navigator/StackNav.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
-  return (
-    <View>
-      <Text>Navigator</Text>
-    </View>
-  );
-}
+import Home from '../screens/Home';
+import Theater from '../screens/Theater';
+
+const TabNavigator = createBottomTabNavigator({
+    Home: Home,
+    Theater: Theater
+},{
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+            let IconComponent = Ionicons;
+            let iconName;
+            if (routeName === 'Home') {
+                iconName = `ios-home`;
+            } else if (routeName === 'Theater') {
+                iconName = `ios-calendar`;
+            }
+            return <IconComponent name={iconName} size={25} color={tintColor} />;
+          },
+    }),
+    tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+    },
+    initialRouteName: 'Home'
+})
+
+const StackNavigator = createStackNavigator({
+    TabNavigator: TabNavigator
+},{
+    defaultNavigationOptions: ({ navigation }) => ({
+        headerTitle: "XXI"
+    })
+})
+
+export default createAppContainer(StackNavigator);
