43fd2ffd884f9f5bc2a815c1e3f5070baff61960
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2019-11-21 22:35:08 +0800

    Update

--- navigator/StackNav.js
+++ navigator/StackNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 export default function App() {
   return (
